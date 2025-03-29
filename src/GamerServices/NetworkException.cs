1aa55686ece88a993c3d78a8f275abd5db7e9470
Dean Ellis <dellis1972@googlemail.com>
2025-03-29 17:21:01 +0000

    Update Copyright and description

--- src/GamerServices/NetworkException.cs
+++ src/GamerServices/NetworkException.cs
@@ -1,6 +1,8 @@
 ﻿#region License
-/* FNA.Steamworks - XNA4 Xbox Live Reimplementation for Steamworks
+/* MonoGame.Steamworks based on
+ * FNA.Steamworks - XNA4 Xbox Live Reimplementation for Steamworks
  * Copyright 2016 Ethan "flibitijibibo" Lee
+ * Copyright 2025 Dean "dellis1972" Ellis
  *
  * Released under the Microsoft Public License.
  * See LICENSE for details.
